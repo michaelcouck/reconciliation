ea1dbbb4a8a19976b862293aaee37201c17c3e5e
Couck Michael <michael.couck@belgiantrain.be>
2022-12-01 15:45:47 +0100

    Finished the commercial report. Now the reconciliation report to finish, formatting in the UI essentially as the logic is done.

--- src/main/java/be/stib/maas/reconciliation/service/ReportGeneratorService.java
+++ src/main/java/be/stib/maas/reconciliation/service/ReportGeneratorService.java
@@ -1,7 +1,6 @@
 package be.stib.maas.reconciliation.service;
 
 import be.stib.maas.reconciliation.model.Dataset;
-import be.stib.maas.reconciliation.model.ReportRequest;
 import be.stib.maas.reconciliation.report.CommercialReportGenerator;
 import be.stib.maas.reconciliation.report.ReconciliationReportGenerator;
 import be.stib.maas.reconciliation.toolkit.DATASET_GENERATOR;
@@ -13,7 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 @RestController
@@ -33,23 +35,37 @@ public class ReportGeneratorService {
     }
 
     @ResponseBody
-    @RequestMapping(value = ReportGeneratorService.COMMERCIAL_REPORT, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> commercialReport(@RequestBody final ReportRequest reportRequest) {
+    @RequestMapping(
+            value = ReportGeneratorService.COMMERCIAL_REPORT,
+            method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ResponseEntity<String> commercialReport(
+            @RequestParam("msp") final String msp,
+            @RequestParam("startDate") final String startDate,
+            @RequestParam("endDate") final String endDate) throws ParseException {
         // TODO : Get the data from Trafi API for these dates
-        Dataset dataset = DATASET_GENERATOR.getDataset(reportRequest.getMsp(), reportRequest.getStartDate(), reportRequest.getEndDate());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Dataset dataset = DATASET_GENERATOR.getDataset(msp, simpleDateFormat.parse(startDate), simpleDateFormat.parse(endDate));
         String reportHtml = transactionsReportGenerator.process(dataset);
         return new ResponseEntity<>(reportHtml, HttpStatus.OK);
     }
 
     @ResponseBody
-    @RequestMapping(value = ReportGeneratorService.RECONCILIATION_REPORT, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> reconciliationReport(@RequestBody final ReportRequest reportRequest, @RequestParam("ingenico-file") final MultipartFile ingenicoFile) {
+    @RequestMapping(value = ReportGeneratorService.RECONCILIATION_REPORT, method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> reconciliationReport(
+            @RequestParam("msp") final String msp,
+            @RequestParam("startDate") final String startDate,
+            @RequestParam("endDate") final String endDate,
+            @RequestParam("ingenico-file") final MultipartFile multipartFormData) throws IOException {
+        log.info("File data : {}", new String(multipartFormData.getBytes()));
         // TODO : Get the data from Trafi API for these dates
-        Dataset datasetOne = DATASET_GENERATOR.getDataset(reportRequest.getMsp(), reportRequest.getStartDate(), reportRequest.getEndDate());
+        // Dataset datasetOne = DATASET_GENERATOR.getDataset(msp, startDate, endDate);
         // TODO : Build the dataset from the Ingenico data
-        Dataset datasetTwo = DATASET_GENERATOR.getDataset(reportRequest.getMsp(), reportRequest.getStartDate(), reportRequest.getEndDate());
-        String[] reportsHtml = reconciliationReportGenerator.process(new Dataset[] {datasetOne, datasetTwo});
-        return new ResponseEntity<>(Arrays.toString(reportsHtml), HttpStatus.OK);
+        // Dataset datasetTwo = DATASET_GENERATOR.getDataset(reportRequest.getMsp(), reportRequest.getStartDate(), reportRequest.getEndDate());
+        // String[] reportsHtml = reconciliationReportGenerator.process(new Dataset[] {datasetOne, datasetTwo});
+        // return new ResponseEntity<>(Arrays.toString(reportsHtml), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
--- src/main/resources/public/js/custom.js
+++ src/main/resources/public/js/custom.js
@@ -4,20 +4,37 @@ function setContent(element, page) {
     section.load(page);
 }
 
-function generateReport(msp, startDate, endDate) {
-    const reportRequest = {
-        'msp': msp.value,
-        'startDate': startDate.value,
-        'endDate': endDate.value
-    };
-    const reportRequestJson = JSON.stringify(reportRequest);
-    // noinspection JSUnresolvedFunction
+function setElementContent(element, content) {
+    const section = $("#" + element);
+    section.children().remove();
+    // section.innerHTML = content;
+    $('#' + element).html('\'' + content + '\'');
+}
+
+function generateCommercialReport() {
+    const form = document.querySelector('#commercial-report-form');
+    const formData = new FormData(form);
     axios.post('/report/commercial-report',
-        reportRequestJson,
-        {headers: {'Content-Type': 'application/json'}}
+        formData,
+        {headers: {'Content-Type': 'multipart/form-data', 'Accept': 'text/html'}}
     ).then(function (response) {
-        let reportWindow = window.open("about:blank", "hello", "width=1140,height=720");
-        reportWindow.document.write(response.data);
+        console.log("Response data : " + response.data);
+        setElementContent("report-result", response.data);
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+function generateReconciliationReport() {
+    const form = document.querySelector('#reconciliation-report-form');
+    const formData = new FormData(form);
+    axios.post('/report/reconciliation-report',
+        formData,
+        {headers: {'Content-Type': 'multipart/form-data'}}
+    ).then(function (response) {
+        /*let reportWindow = window.open("about:blank", "hello", "width=1140,height=720");
+        reportWindow.document.write(response.data);*/
+        setContent("report-result", response.data);
     }).catch(function (error) {
         console.log(error);
     });
